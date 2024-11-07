cafaf2c8d8b23d95fd15e58a4940a75ac1b01914
LongoVital <vitallongo@gmail.com>
2024-11-07 16:38:30 -0300

    nicooo ahi esta

--- BD/proyectonico.sql
+++ BD/proyectonico.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-11-2024 a las 21:19:01
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 07-11-2024 a las 20:36:39
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 5.6.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `finalnico`
+-- Base de datos: `proyectonico`
 --
 
 -- --------------------------------------------------------
@@ -29,61 +30,29 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `alumnos` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
-  `fecnac` date NOT NULL
+  `nombre` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
+  `apellido` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
+  `fec_nac` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `alumnos`
 --
 
-INSERT INTO `alumnos` (`id`, `nombre`, `apellido`, `fecnac`) VALUES
-(5, 'David', 'Carletta', '1988-11-11'),
-(6, 'Lukitas', 'Del Capo', '2222-05-05'),
-(7, 'Alejo', 'Herrera', '2000-10-19'),
-(8, 'Vital', 'Longo', '1111-11-11'),
-(9, 'Dario', 'Flores', '1111-11-11'),
-(10, 'Maksi', 'Alcaraz', '1111-11-11'),
-(11, 'Nico', 'Velez', '1111-11-11'),
-(12, 'Valentina', 'Danti', '1111-11-11'),
-(13, 'Sara', 'Meliti', '1111-11-11'),
-(14, 'Stefi', 'Saiquita', '1111-11-11'),
-(15, 'Amiel', 'Arizaga', '1111-11-11'),
-(16, 'Elian', 'Arizaga', '1111-11-11'),
-(17, 'Joa', 'Borras', '1111-11-11'),
-(18, 'Alan', 'Velasquez', '1111-11-11'),
-(19, 'Nazareno', 'Serra', '1111-11-11'),
-(20, 'Geronimo', 'Aguirres', '2002-11-19'),
-(21, 'Mateo', 'Bazan', '1111-11-11'),
-(22, 'Valentino', 'Cejas', '1111-11-11'),
-(23, 'Santiago', 'Fiore', '1111-11-11'),
-(24, 'Ivo', 'Romero', '1111-11-11'),
-(25, 'Gaspar', 'Rinaldi', '1111-11-11'),
-(26, 'Mauricio', 'Rapari', '1111-11-11'),
-(27, 'Martin', 'Sequeira', '1111-11-11'),
-(28, 'Agustin', 'De Sensi', '1111-11-11');
+INSERT INTO `alumnos` (`id`, `nombre`, `apellido`, `fec_nac`) VALUES
+(7, 'vital', 'longo', '0055-05-05 00:00:00');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `alumno_materia`
+-- Estructura de tabla para la tabla `alumnos_materias`
 --
 
-CREATE TABLE `alumno_materia` (
+CREATE TABLE `alumnos_materias` (
   `id` int(11) NOT NULL,
   `alumno_id` int(11) NOT NULL,
   `materia_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `alumno_materia`
---
-
-INSERT INTO `alumno_materia` (`id`, `alumno_id`, `materia_id`) VALUES
-(37, 5, 6),
-(38, 5, 9),
-(39, 5, 5);
 
 -- --------------------------------------------------------
 
@@ -93,22 +62,15 @@ INSERT INTO `alumno_materia` (`id`, `alumno_id`, `materia_id`) VALUES
 
 CREATE TABLE `materias` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL
+  `nombre_materia` varchar(255) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `materias`
 --
 
-INSERT INTO `materias` (`id`, `nombre`) VALUES
-(2, 'Redes y comunicaciones'),
-(3, 'Seguridad de los sistemas'),
-(4, 'Sistema de informacion organizacional'),
-(5, 'Desarrollo de sistemas web'),
-(6, 'Base de datos'),
-(9, 'Derecho y legislacion laboral'),
-(10, 'Etica y responsabilidad social'),
-(11, 'Practicas Profesionalizantes II');
+INSERT INTO `materias` (`id`, `nombre_materia`) VALUES
+(6, 'Base de Datos');
 
 -- --------------------------------------------------------
 
@@ -118,46 +80,35 @@ INSERT INTO `materias` (`id`, `nombre`) VALUES
 
 CREATE TABLE `profesores` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(11) NOT NULL,
-  `apellido` varchar(11) NOT NULL,
-  `materia_id` int(11) NOT NULL
+  `nombre` varchar(255) COLLATE utf8_spanish2_ci NOT NULL,
+  `apellido` varchar(255) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `profesores`
 --
 
-INSERT INTO `profesores` (`id`, `nombre`, `apellido`, `materia_id`) VALUES
-(2, 'Walt', 'Bur', 4),
-(3, 'Nico', 'Rotilli', 5),
-(4, 'Karina ', 'Gigli', 11),
-(5, 'Omar', 'Orlandini', 10),
-(6, 'Viviana', 'Romo', 9);
+INSERT INTO `profesores` (`id`, `nombre`, `apellido`) VALUES
+(5, 'nico', 'rotili');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `profesor_materia`
+-- Estructura de tabla para la tabla `profesores_materias`
 --
 
-CREATE TABLE `profesor_materia` (
-  `profesor_id` int(11) NOT NULL,
-  `materia_id` int(11) NOT NULL
+CREATE TABLE `profesores_materias` (
+  `id` int(11) NOT NULL,
+  `profesores_id` int(11) NOT NULL,
+  `materias_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `profesor_materia`
+-- Volcado de datos para la tabla `profesores_materias`
 --
 
-INSERT INTO `profesor_materia` (`profesor_id`, `materia_id`) VALUES
-(2, 2),
-(2, 3),
-(2, 4),
-(3, 6),
-(3, 5),
-(4, 11),
-(5, 10),
-(6, 9);
+INSERT INTO `profesores_materias` (`id`, `profesores_id`, `materias_id`) VALUES
+(6, 5, 6);
 
 --
 -- Índices para tablas volcadas
@@ -170,10 +121,9 @@ ALTER TABLE `alumnos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `alumno_materia`
+-- Indices de la tabla `alumnos_materias`
 --
-ALTER TABLE `alumno_materia`
-  ADD PRIMARY KEY (`id`),
+ALTER TABLE `alumnos_materias`
   ADD KEY `alumno_id` (`alumno_id`),
   ADD KEY `materia_id` (`materia_id`);
 
@@ -187,15 +137,15 @@ ALTER TABLE `materias`
 -- Indices de la tabla `profesores`
 --
 ALTER TABLE `profesores`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `materia_id` (`materia_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `profesor_materia`
+-- Indices de la tabla `profesores_materias`
 --
-ALTER TABLE `profesor_materia`
-  ADD KEY `materia_id` (`materia_id`),
-  ADD KEY `profesor_id` (`profesor_id`);
+ALTER TABLE `profesores_materias`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profesores_id` (`profesores_id`),
+  ADD KEY `materias_id` (`materias_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -205,43 +155,43 @@ ALTER TABLE `profesor_materia`
 -- AUTO_INCREMENT de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
-
---
--- AUTO_INCREMENT de la tabla `alumno_materia`
---
-ALTER TABLE `alumno_materia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `materias`
 --
 ALTER TABLE `materias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `profesores`
 --
 ALTER TABLE `profesores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `profesores_materias`
+--
+ALTER TABLE `profesores_materias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `alumno_materia`
+-- Filtros para la tabla `alumnos_materias`
 --
-ALTER TABLE `alumno_materia`
-  ADD CONSTRAINT `alumno_materia_ibfk_1` FOREIGN KEY (`alumno_id`) REFERENCES `alumnos` (`id`),
-  ADD CONSTRAINT `alumno_materia_ibfk_2` FOREIGN KEY (`materia_id`) REFERENCES `materias` (`id`);
+ALTER TABLE `alumnos_materias`
+  ADD CONSTRAINT `alumnos_materias_ibfk_1` FOREIGN KEY (`alumno_id`) REFERENCES `alumnos` (`id`),
+  ADD CONSTRAINT `alumnos_materias_ibfk_2` FOREIGN KEY (`materia_id`) REFERENCES `materias` (`id`);
 
 --
--- Filtros para la tabla `profesor_materia`
+-- Filtros para la tabla `profesores_materias`
 --
-ALTER TABLE `profesor_materia`
-  ADD CONSTRAINT `profesor_materia_ibfk_1` FOREIGN KEY (`materia_id`) REFERENCES `materias` (`id`),
-  ADD CONSTRAINT `profesor_materia_ibfk_2` FOREIGN KEY (`profesor_id`) REFERENCES `profesores` (`id`);
+ALTER TABLE `profesores_materias`
+  ADD CONSTRAINT `profesores_materias_ibfk_1` FOREIGN KEY (`profesores_id`) REFERENCES `profesores` (`id`),
+  ADD CONSTRAINT `profesores_materias_ibfk_2` FOREIGN KEY (`materias_id`) REFERENCES `materias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
